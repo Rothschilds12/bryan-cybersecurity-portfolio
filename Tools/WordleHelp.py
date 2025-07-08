f25719f284f5bd3e2ab0cc41f7d3607ae1d75874
Dreamcrusher500 <bryan.pfeifer7@gmail.com>
2025-07-08 16:26:41 -0400

    added included letter and letter frequency

--- Tools/WordleHelp.py
+++ Tools/WordleHelp.py
@@ -1,9 +1,19 @@
 import string
 
 word_length = 5
-
 fixed_index = []
 fixed_char = []
+greatest_frequency = 0
+most_likely_word = None
+
+inverse_frequency_score = {
+    'e': 26, 't': 25, 'a': 24, 'o': 23, 'i': 22,
+    'n': 21, 's': 20, 'h': 19, 'r': 18, 'd': 17,
+    'l': 16, 'c': 15, 'u': 14, 'm': 13, 'w': 12,
+    'f': 11, 'g': 10, 'y': 9,  'p': 8,  'b': 7,
+    'v': 6,  'k': 5,  'j': 4,  'x': 3,  'q': 2,  'z': 1
+}
+
 
 
 def load_words(filename="words_alpha.txt"):
@@ -13,21 +23,34 @@ def load_words(filename="words_alpha.txt"):
 english_words = load_words()
 
 
-def check(input_value):
-    return input_value.lower() in english_words
+def check(input_value, included_letter):
+    count = 0
+    global greatest_frequency, most_likely_word
+    temp = 0
+    for i in input_value:
+        temp += inverse_frequency_score.get(i)
+        if i in included_letter:
+            count += 1
+    if input_value.lower() in english_words and count == len(included_letter):
+        if temp > greatest_frequency:
+            greatest_frequency = temp
+            most_likely_word = input_value.lower()
+        return True
+    else:
+        return False
 
-def brute_force(current, position, letters):
+def brute_force(current, position, letters, included_letter):
     if position == word_length:
-        if check(current):
+        if check(current, included_letter):
             print(current)
         return
 
     if position in fixed_index:
         idx = fixed_index.index(position)
-        brute_force(current + fixed_char[idx], position + 1, letters)
+        brute_force(current + fixed_char[idx], position + 1, letters, included_letter)
     else:
         for letter in letters:
-            brute_force(current + letter, position + 1, letters)
+            brute_force(current + letter, position + 1, letters, included_letter)
 
 def main():
     letters = string.ascii_lowercase
@@ -38,6 +61,7 @@ def main():
 
         input_guess = input("Word guess (use _ for unknowns): ").strip().lower()
         excluded_letter = input("enter excluded letters:")
+        included_letter = input("enter included letters:")
 
         for i in excluded_letter:
             letters = letters.replace(i,"")
@@ -54,7 +78,11 @@ def main():
                 fixed_char.append(char)
 
         print("Searching for valid words...")
-        brute_force("", 0, letters)
+        brute_force("", 0, letters, included_letter)
+        print("=========================================================================")
+        print(f"Most Try this next\t{most_likely_word}")
+        print("=========================================================================")
+
 
 if __name__ == "__main__":
     main()
